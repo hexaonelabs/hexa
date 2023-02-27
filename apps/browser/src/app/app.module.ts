13132f83ac60c0a27e7d7a287c696e31630fe2b3
FazioNico <contact@nicolasfazio.ch>
2023-02-27 09:56:26 +0100

    feat(browser): add pwa support

--- apps/browser/src/app/app.module.ts
+++ apps/browser/src/app/app.module.ts
@@ -1,28 +1,38 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BehaviorSubject, debounceTime, distinctUntilChanged } from 'rxjs';
 import { IonicModule } from '@ionic/angular';
+import { CeramicClient } from '@ceramicnetwork/http-client';
+
+import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { DashboardComponent } from './dashboard.component';
-import { BehaviorSubject, debounceTime, distinctUntilChanged } from 'rxjs';
-import { CommonModule } from '@angular/common';
+import { AppGuard } from './app.guard';
 import { Web3AuthService } from './web3-auth.service';
-import { CeramicClient } from '@ceramicnetwork/http-client';
 import { DIDService } from './did.service';
 import { DatastoreService } from './datastore.service';
-import { AppGuard } from './app.guard';
-import { environment } from '../environments/environment';
 import { IPFSService } from './ipfs.service';
 import { PinataService } from './pinata.service';
 import { NotificationService } from './notification.service';
 import { EncryptionService } from './encryption.service';
 import { XMTPService } from './messaging.service';
+import { GlobalErrorHandlerService } from './global-error-handler.service';
+import { environment } from '../environments/environment';
 import { ILoadingService } from '@d-workspace/interfaces';
 
 const CERAMIC_CLIENT = new CeramicClient('https://ceramic-clay.3boxlabs.com');
 const LOADERCLIENT = new BehaviorSubject<boolean>(false);
+const ERROR_PROVIDER = 
+  environment.production
+    ? [{
+        provide: ErrorHandler,
+        useClass: GlobalErrorHandlerService,
+      }]
+    : [];
 
 @NgModule({
   declarations: [
@@ -36,10 +46,20 @@ const LOADERCLIENT = new BehaviorSubject<boolean>(false);
       mode: 'ios'
     }),
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' , useHash: true }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [
     AppGuard,
     XMTPService,
+    ...ERROR_PROVIDER,
     {
       provide: 'APP_LOADER_SERVICE',
       useFactory: (): ILoadingService => {
