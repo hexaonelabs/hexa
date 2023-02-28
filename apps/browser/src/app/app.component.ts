27835edd9e6a21c691aae1bebcc60a88043b6f13
FazioNico <contact@nicolasfazio.ch>
2023-02-28 23:14:41 +0100

    fix: add missing import

--- apps/browser/src/app/app.component.ts
+++ apps/browser/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ILoadingService } from '@d-workspace/interfaces';
 import { environment } from '../environments/environment';
+import '@khmyznikov/pwa-install';
 
 @Component({
   selector: 'd-workspace-root',
