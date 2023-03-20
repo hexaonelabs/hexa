39397d72465bf99d6b244cd8f7eaddf08643908c
FazioNico <contact@nicolasfazio.ch>
2023-03-20 17:01:16 +0100

    fix: #close 22

--- libs/interfaces/src/lib/services/auth-service.interface.ts
+++ libs/interfaces/src/lib/services/auth-service.interface.ts
@@ -1,23 +1,22 @@
 import { BehaviorSubject, distinctUntilChanged, filter, firstValueFrom, map, switchMap } from "rxjs";
 import { ethers } from 'ethers';
+import { DID } from '@d-workspace/dids';
+
+export interface IAuthUser {
+  latestConnectionISODatetime?: string; 
+  latestNotifedISODatetime?: string;
+  creationISODatetime?: string;
+}
 
 export interface IAuthService {
   ethereumProvider$: BehaviorSubject<ethers.providers.Web3Provider>;
   signer$: BehaviorSubject<ethers.providers.JsonRpcSigner>;
   account$: BehaviorSubject<string>;
-  did$: BehaviorSubject<any>;
+  profile$: BehaviorSubject<IAuthUser>;
+  did$: BehaviorSubject<DID>;
   connect(redirectUrl?: string): Promise<boolean>;
   disconnect(): Promise<void>;
   getAccountDID(address: string, chainParam?: string): Promise<string>;
-  getProfilData(): Promise<{
-    latestConnectionISODatetime: string;
-    latestNotifedISODatetime?: string;
-  }>;
-  updateProfilData(data: Partial<{
-    latestConnectionISODatetime: string;
-    latestNotifedISODatetime: string;
-  }>): Promise<{
-    latestConnectionISODatetime: string;
-    latestNotifedISODatetime?: string;
-  }>;
+  getProfilData(): Promise<IAuthUser>;
+  updateProfilData(data: Partial<IAuthUser>): Promise<IAuthUser>;
 }
