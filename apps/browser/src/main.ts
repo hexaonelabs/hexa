922fc3867a750a9861b02cc65ced05e4a2ef5edf
FazioNico <contact@nicolasfazio.ch>
2023-03-31 21:59:48 +0200

    refactor: disable logs

--- apps/browser/src/main.ts
+++ apps/browser/src/main.ts
@@ -5,11 +5,22 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const disableConsole = () => {
+  // disabling console.log in production
+  window.console.log = (...data: any[]) => ({});
+  // disabling console.warn in production
+  window.console.warn = (...data: any[]) => ({});
+  // disabling console.error in production
+  window.console.error = (...data: any[]) => ({});
+  // disabling console.info in production
+  window.console.info = (...data: any[]) => ({});
+  // disabling console.debug in production
+  window.console.debug = (...data: any[]) => ({});
+}
+
 if (environment.production) {
   enableProdMode();
-  window.console.log = (...data: any[]) => {
-    // disabling console.log in production
-  };
+  disableConsole();
 }
 
 platformBrowserDynamic()
